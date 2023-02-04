3d57d5968f43da243aaed93a8d49d24b73520df5
User <michaelm810129@gmail.com>
2023-02-04 17:33:02 +1030

    function/added dates and weather icons to cards

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -14,32 +14,31 @@ function getWeather (){
   submit.addEventListener('click', ()=>{
     let city = cityName.value;
     
-    console.log(city);
+    
     fetch('https://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&appid='+apiKey)
     .then(response => response.json())
     .then(data=> {
         console.log(data);
         var lat = data.coord.lat;
         var lon = data.coord.lon
-        console.log(lat);
-        console.log(lon);
+        
         // Adds City Name to the card title for current weather 
       let currentCityWeather = document.querySelector('.card-title');
       currentCityWeather.textContent = data.name;
       // Adds current temp 
       let currentTemp = document.querySelector('.tempNow');
-      currentTemp.textContent = 'Current Temp - ' + data.main.temp;
+      currentTemp.textContent = 'Current Temp: ' + data.main.temp;
       // Adds weather icon for current temp 
       let weatherIcon = document.getElementById('currentImg');
       let icon = data.weather[0].icon;
-      console.log(icon);
+      
       let iconUrl = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
       weatherIcon.src = iconUrl;
       // Add datas to cards
       let currentDt = data.dt;
       let date = new Date(currentDt*1000);
       let formattedDate = date.toLocaleDateString();
-      console.log(formattedDate);
+      
       let dateBox = document.querySelector('.currentDate');
       dateBox.textContent = formattedDate;
       
@@ -55,14 +54,11 @@ function getWeather (){
   function fiveDayForecast(){
         submit.addEventListener('click', ()=>{
           const city = cityName.value;
-          fetch('https://api.openweathermap.org/data/2.5/forecast?q='+city+'&units=metric&appid='+apiKey)
+          fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=metric&appid='+apiKey)
           .then(response => response.json())
           .then (data => {
             console.log(data);
-            console.log(data.list[8].dt);
-            console.log(data.list[16].dt);
-            console.log(data.list[24].dt);
-            console.log(data.list[32].dt);
+            // Object holding day/time Unix stamps
             const Days = {
               day1: data.list[0].dt,
               day2: data.list[8].dt,
@@ -70,48 +66,54 @@ function getWeather (){
               day4: data.list[24].dt,
               day5: data.list[32].dt,
               
-            };
+            }
            
            
-            // NOT WORKING YET - CREATING ICONS FOR WEATHER BOXES
+            // Loop to add data to each cardDates, may change to display date and time 
             const cardDates = document.querySelectorAll('.cardDate');
             for (var i = 0; i < cardDates.length; i++){
               const current = Days[Object.keys(Days)[i]];
               let date = new Date(current * 1000);
               let formattedDate =date.toLocaleDateString();
-              cardDates[i].textContent = formattedDate;
+                cardDates[i].textContent = formattedDate;
+                console.log(data.list.dt_txt);
             }
-            const forecastIcon = document.createElement('img');
-            forecastIcon.className += ("Icons");
-            icons = data.list[i].weather[0].icon;
-            const iconsUrl = "https://openweathermap.org/img/wn/"+icons+"@2x.png";
-            forecastIcon.src = iconsUrl;
-            forecastIcon.append(cardDates);
             
-          })
-        })
-  }
-}
+            
+            
+            // Loop to add weather icons to each card 
+            for (var j = 0; j < cardDates.length; j ++) {
+            const forecastIcon = document.createElement('img');
+            forecastIcon.setAttribute('id', "castIcon");
+            icons = data.list[j].weather[0].icon;
+            
+            const iconsUrl = "https://openweathermap.org/img/wn/" + icons + "@2x.png";
+            forecastIcon.src = iconsUrl;
+           
+            cardDates[j].append(forecastIcon);
+            }
 
+            // Object and loop to display max temp for the day 
+              console.log(data.list[0].main.temp_max)
+            const dailyTemp = document.querySelectorAll('.dailyTemp');
+            const Temp = {
+              temp1: data.list[0].main.temp_max,
+              temp2: data.list[8].main.temp_max,
+              temp3: data.list[16].main.temp_max,
+              temp4: data.list[24].main.temp_max,
+              temp5: data.list[32].main.temp_max,
+            };
+            
+            console.log(Temp);
+            for(var h = 0; h < dailyTemp.length; h ++) {
+              dailyTemp[h].innerHTML = "Max Temp: " + Temp[Object.keys(Temp)[h]];
+            }
 
+            
+            
+            
+            
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+          
+        }
+        )})}}
